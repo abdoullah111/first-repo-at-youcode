5816fb2d119903f8657adab3f8bad93b45960ca9
abdoullah111 <piktas1337@gmail.com>
2026-09-14 13:25:27 +0200

    day05

---
+++ day04/exercices/exercice-12.js
@@ -0,0 +1,22 @@
+/**
+ * ─────────────────────────────────────────────────────────────
+ * JOUR 04 · EXERCICE 12 · NIVEAU 2 : CONSOLIDATION (INTERMÉDIAIRES)
+ * INVERSION DE MOTS DANS UNE PHRASE
+ * ─────────────────────────────────────────────────────────────
+ *
+ * 🎯 MISSION
+ * Prenez la phrase "Bonjour le monde" et transformez-la en "monde le Bonjour".
+ *
+ * 📖 Consigne détaillée : ../03-exercices.md#exercice-12
+ * ▶️ Commande : node day04/exercices/exercice-12.js
+ */
+'use strict';
+
+// 1. Identifie les données nécessaires.
+// 2. Écris ta solution sous cette ligne.
+// TODO: écris ta solution ici.
+let phrase = "Bonjour le monde";
+
+let reverse= phrase.split(" ").reverse().join(" ");
+
+console.log(reverse)
