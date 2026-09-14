808bbc7882d8d6d8024f19b13fe8eb2439d2e7c3
abdoullah111 <piktas1337@gmail.com>
2026-09-14 16:24:21 +0200

    day05

--- day04/exercices/exercice-16.js
+++ day04/exercices/exercice-16.js
@@ -5,7 +5,8 @@
  * ─────────────────────────────────────────────────────────────
  *
  * 🎯 MISSION
- * À partir d'un tweet : "Beau temps à #Nador aujourd'hui avec la team #YouCode !", extrayez tous les hashtags et retournez-les dans un tableau ["#Nador", "#YouCode"].
+ * À partir d'un tweet : "Beau temps à #Nador aujourd'hui avec la team #YouCode !", 
+ * extrayez tous les hashtags et retournez-les dans un tableau ["#Nador", "#YouCode"].
  *
  * 📖 Consigne détaillée : ../03-exercices.md#exercice-16
  * ▶️ Commande : node day04/exercices/exercice-16.js
@@ -15,3 +16,14 @@
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
 // TODO: écris ta solution ici.
+let tweet = "Beau temps à #Nador aujourd'hui avec la team #YouCode !";
+let splitTweet = tweet.split(" ");
+let hashtag = [];
+// console.log(splitTweey)
+// console.log(splitTweey[0][0])
+for(let i = 0; i<splitTweey.length;i++){
+    if(splitTweet[i][0] == "#"){
+        hashtag.push(splitTweet[i])
+    }
+}
+console.log(hashtag)
