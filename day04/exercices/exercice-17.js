8936fca74d331b241e186186db5aa6a2d7de09d0
abdoullah111 <piktas1337@gmail.com>
2026-09-15 15:55:00 +0200

    day05

--- day04/exercices/exercice-17.js
+++ day04/exercices/exercice-17.js
@@ -1,7 +1,7 @@
 /**
  * ─────────────────────────────────────────────────────────────
  * JOUR 04 · EXERCICE 17 · NIVEAU 3 : DÉFI (AVANCÉS)
- * LE CHIFFRE DE CÉSAR (CRYPTOGRAPHIE)
+ * LE CHIFFRE DE CÉSAR (CRYPTOG7RAPHIE)
  * ─────────────────────────────────────────────────────────────
  *
  * 🎯 MISSION
@@ -15,3 +15,27 @@
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
 // TODO: écris ta solution ici.
+function chiffrerCesar(texte, decalage){
+    let result = "";
+    for(let i = 0; i<= texte.length; i++){
+        let num = texte.charCodeAt(i)// 65
+        if(num >= 65 && num<= 90){
+            let newNum = num - 65;
+            let core = (newNum + decalage)%26;
+            let final = core + 65
+            result += String.fromCharCode(final)
+        }else if(num>=97 && num <= 122 ){
+            let number = num - 97;
+            let core1 = (number + decalage)%26;
+            let final1 = core1 + 97;
+            result+= String.fromCharCode(final1)
+        }
+
+        
+        
+
+    }
+    return result
+
+}
+console.log(chiffrerCesar("a", 3))
