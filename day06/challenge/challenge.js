261322bccaafe8997a524b92208ee83b45d9a83f
abdoullah111 <piktas1337@gmail.com>
2026-09-16 17:53:56 +0200

    challenge-day06

--- day06/challenge/challenge.js
+++ day06/challenge/challenge.js
@@ -21,3 +21,44 @@
 
 // Découpe d'abord le problème en petites étapes.
 // TODO: écris ta solution ici.
+let utilisateurs = [];
+let id = 1
+function ajouterUtilisateur(nom, email){
+    let user = {
+        id: "ID-" + id,
+        nom: nom,
+        email: email,
+    }
+    utilisateurs.push(user)
+    id++
+}
+function trouverParEmail(email){
+    for ( let i = 0; i < utilisateurs.length; i++){
+        if (utilisateurs[i].email === email) {
+        return utilisateurs[i];
+       }     
+     }
+    return null;
+    }
+function supprimerParId(id){
+    for (let x = 0; x < utilisateurs.length; x++){
+        if(utilisateurs[x].id === id){
+            utilisateurs.splice(x, 1)
+        }
+    }
+}
+
+function afficherAnnuaire(){
+    for(let y = 0; y < utilisateurs.length; y++){
+        if (utilisateurs[y] !== null){
+            console.log(utilisateurs[y]);
+        }
+    }
+}
+
+
+
+
+ajouterUtilisateur("farnese", "beinjinglover00@gmail.com");
+ajouterUtilisateur("riadeino", "fakestriadeino@gmail.com");
+afficherAnnuaire()
