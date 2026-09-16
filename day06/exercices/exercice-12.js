a451abe37935c99c0a5e881608ea50f8bd68be1f
abdoullah111 <piktas1337@gmail.com>
2026-09-16 12:16:10 +0200

    day06

--- day06/exercices/exercice-12.js
+++ day06/exercices/exercice-12.js
@@ -5,7 +5,8 @@
  * ─────────────────────────────────────────────────────────────
  *
  * 🎯 MISSION
- * Créez un objet compteBancaire avec une propriété solde et une méthode deposer(montant). La méthode doit utiliser this.solde pour augmenter le solde actuel.
+ * Créez un objet compteBancaire avec une propriété solde et une méthode deposer(montant).
+ *  La méthode doit utiliser this.solde pour augmenter le solde actuel.
  *
  * 📖 Consigne détaillée : ../03-exercices.md#exercice-12
  * ▶️ Commande : node day06/exercices/exercice-12.js
@@ -15,3 +16,15 @@
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
 // TODO: écris ta solution ici.
+let compteBancaire = {
+    solde: 100,
+    deposer: function(montant){
+        this.solde +=montant
+        return this.solde
+    }
+    
+
+}
+console.log(compteBancaire.solde)
+console.log(compteBancaire.deposer(50))
+
